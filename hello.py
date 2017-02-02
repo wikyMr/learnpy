3bf5a903df96af88d1b5ef1b3ec9404b3e1075ce
wiky <szucyw@gmail.com>
2017-02-02 14:44:53 +0800

    learning py.

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Please input your name:");
+name=input();
+print("hello,python!",name);
---
+++ pythonBasic.py
@@ -0,0 +1,31 @@
+'''判断语句
+age1 = 15;
+if age1>=18:
+	print("adult")
+else:
+	print('not adult')
+'''
+#list
+classmate=['a','b','c'];
+#print(classmate[0]);#a
+classmate.append("d");
+#print(classmate[2]);
+
+#tuple
+mytuple=(1,);
+#mytuple[0]=2;#error
+#print(mytuple[0]);
+
+#判断语句
+age=10;
+if age >5:
+	print("Older than 5.");
+else:
+	print("Yonger than 5.");
+
+#input:
+birth=input("input you birth:");
+if birth>=10:#birth为string类型的：
+	print("Older.");
+else:
+	print("Yonger.");
